015941cebcf6bb9582866d1ba688474dbb4f6872
ishanksainger <isainger29@gmail.com>
2024-01-13 13:39:24 +0530

    modal change

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -12,7 +12,7 @@ const ConfirmationModal = ({modalData}) => {
                 {modalData.text2}
             </p>
             <div className="flex items-center gap-x-4">
-                <IconBtn customClasses="pr-8 pl-8" onClick={modalData ?.btn1Handler}
+                <IconBtn customClasses="pr-8 pl-8" onClick={modalData?.btn1Handler}
                 text={modalData?.btn1Text}/>
                 <button  className="cursor-pointer rounded-md bg-richblack-200 py-[8px] px-[20px] font-semibold text-richblack-900" 
                 onClick={modalData?.btn2Handler}>{modalData?.btn2Text}</button>
